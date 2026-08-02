dfb068a147095ab5b7e39fe7a50e57b79931016b
Zachary <ZacharyScript@gmail.com>
2026-08-01 20:30:58 -0400

    Moved material structs
    Moved material structs from material_system.hpp to materials.hpp

--- src/asset_systems/material_system.hpp
+++ src/asset_systems/material_system.hpp
@@ -8,56 +8,9 @@
 #include "core_engine_types.h"
 #include "tools/synchronized_buffer.hpp"
 #include "texture_loader.hpp"
+#include "materials.hpp"
 #define UFBX_REAL_IS_FLOAT 1
 #include "ufbx.h"
-
-using MaterialHandle = uint16_t;
-using PBRMaterialHandle = uint16_t;
-using ToonMaterialHandle = uint16_t;
-inline constexpr MaterialHandle INVALID_MATERIAL = UINT16_MAX;
-
-enum class MaterialType : uint8_t
-{
-    PBR = 0,
-    Toon = 0,
-    Unknown = (uint8_t)(-1)
-};
-
-struct PBRMaterial
-{
-    MTL::Texture* albedoTexture = nullptr;
-    MTL::Texture* normalTexture = nullptr;
-    MTL::Texture* metallicRoughnessAoTexture = nullptr;
-    MTL::Texture* emission = nullptr;
-
-    simd_float4 baseColorFactor = { 1.0f, 1.0f, 1.0f, 1.0f };
-    float metallicFactor = 1.0f;
-    float roughnessFactor = 1.0f;
-    float ambientOcclusionFactor = 1.0f;
-    simd_float3 emissionFactor = { 0.0f, 0.0f, 0.0f };
-};
-
-struct ToonMaterial
-{
-    MTL::Texture* albedoTexture = nullptr;
-    MTL::Texture* shadowThresholdTexture = nullptr;
-    
-    simd_float4 baseColorFactor = { 1.0f, 1.0f, 1.0f, 1.0f };
-    float shadowThresholdValue = 0.5f;
-    
-    float shadowSoftness = 0.0f;
-};
-
-struct Material
-{
-    MaterialType type = MaterialType::Unknown;
-    bool isTombstone = false;
-    union
-    {
-        PBRMaterial pbrMaterial;
-        ToonMaterial toonMaterial;
-    };
-};
 
 struct MaterialEntry
 {
---
+++ src/asset_systems/materials.hpp
@@ -0,0 +1,50 @@
+/**
+ * @file materials.hpp
+ * @brief Structures for materials
+ */
+
+using MaterialHandle = uint16_t;
+inline constexpr MaterialHandle INVALID_MATERIAL = UINT16_MAX;
+
+enum class MaterialType : uint8_t
+{
+    PBR = 0,
+    Toon = 0,
+    Unknown = (uint8_t)(-1)
+};
+
+struct PBRMaterial
+{
+    MTL::Texture* albedoTexture = nullptr;
+    MTL::Texture* normalTexture = nullptr;
+    MTL::Texture* metallicRoughnessAoTexture = nullptr;
+    MTL::Texture* emission = nullptr;
+
+    simd_float4 baseColorFactor = { 1.0f, 1.0f, 1.0f, 1.0f };
+    float metallicFactor = 1.0f;
+    float roughnessFactor = 1.0f;
+    float ambientOcclusionFactor = 1.0f;
+    simd_float3 emissionFactor = { 0.0f, 0.0f, 0.0f };
+};
+
+struct ToonMaterial
+{
+    MTL::Texture* albedoTexture = nullptr;
+    MTL::Texture* shadowThresholdTexture = nullptr;
+    
+    simd_float4 baseColorFactor = { 1.0f, 1.0f, 1.0f, 1.0f };
+    float shadowThresholdValue = 0.5f;
+    
+    float shadowSoftness = 0.0f;
+};
+
+struct Material
+{
+    MaterialType type = MaterialType::Unknown;
+    bool isTombstone = false;
+    union
+    {
+        PBRMaterial pbrMaterial;
+        ToonMaterial toonMaterial;
+    };
+};
