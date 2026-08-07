7684301b1e2ade9c411cd7df2bfa96bf8ec19121
Zachary <ZacharyScript@gmail.com>
2026-08-07 16:53:58 -0400

    tests passing: adding and replacing textures

--- tests/material_system/material_system_test.cpp
+++ tests/material_system/material_system_test.cpp
@@ -536,21 +536,21 @@ TEST_CASE("updating a material by adding a texture should be reflected in the sy
     std::vector<MaterialHandle> consumedHandles = system.getItemsAndDrainAdditionsOutputBuffer();
     MaterialHandle materialToUpdate = consumedHandles[1];
 
-    MTL::Texture* textureToAdd = textureLoader.loadTexture("assets/test_cube_four_channel_texture.png");
-    REQUIRE(system.materials[materialToUpdate].pbrMaterial.albedoTexture != textureToAdd);
+    MTL::Texture* textureToAdd = textureLoader.loadTexture("assets/test_cube_texture.png");
+    REQUIRE(system.materials[materialToUpdate].pbrMaterial.metallicRoughnessAoTexture != textureToAdd);
 
     // When the call is made to update the material texture
-    system.updateMaterialTexture(materialToUpdate, (MaterialTextureOffset::TextureOffset)MaterialTextureOffset::PBRTextureOffset::Albedo, textureToAdd);
+    system.updateMaterialTexture(materialToUpdate, (MaterialTextureOffset::TextureOffset)MaterialTextureOffset::PBRTextureOffset::ORM, textureToAdd);
     // The texture updates input buffer should have one item
     REQUIRE(1 == system.textureUpdatesInputBuffer.count);
-    REQUIRE(textureToAdd != system.materials[materialToUpdate].pbrMaterial.albedoTexture);
+    REQUIRE(textureToAdd != system.materials[materialToUpdate].pbrMaterial.metallicRoughnessAoTexture);
 
     // When the updates input buffer is drained
     system.drainUpdatesInputBuffers();
     // The texture updates input buffer should have zero items
     REQUIRE(0 == system.textureUpdatesInputBuffer.count);
 
-    REQUIRE(textureToAdd == system.materials[materialToUpdate].pbrMaterial.albedoTexture);
+    REQUIRE(textureToAdd == system.materials[materialToUpdate].pbrMaterial.metallicRoughnessAoTexture);
 
     // Cleaning up
     system.drainRemovalsInputBuffer();
@@ -675,6 +675,57 @@ TEST_CASE("texture updates should take precedence over material updates when a m
     autoReleasePool->release();
 }
 
-TEST_CASE("updating a material by replacing a texture should be reflected in the system", "[material][asset system][update][fbx][metal]") {}
+TEST_CASE("updating a material by replacing a texture should be reflected in the system", "[material][asset system][update][fbx][metal]")
+{
+    NS::AutoreleasePool* autoReleasePool = NS::AutoreleasePool::alloc()->init();
+    MTL::Device* metalDevice = MTL::CreateSystemDefaultDevice();
+
+    TextureLoader textureLoader(metalDevice);
+    MaterialSystem system(&textureLoader);
+    ufbx_scene* scene = aSceneWithATexturedCube();
+    ufbx_node* cubeNode = aNodeWithAGivenNameInAScene(scene, "Cube");
+    REQUIRE(nullptr != cubeNode);
+
+    std::vector<size_t> albedoMaterialIndices = indicesInAListWithAnAlbedoTexture(cubeNode->materials);
+    std::vector<size_t> normalMaterialIndices = indicesInAListWithANormalTexture(cubeNode->materials);
+    std::vector<size_t> emissionMaterialIndices = indicesInAListWithAnEmissionTexture(cubeNode->materials);
+    REQUIRE(0 < albedoMaterialIndices.size());
+    REQUIRE(0 < normalMaterialIndices.size());
+    REQUIRE(0 < emissionMaterialIndices.size());
+    
+    std::vector<MaterialHandle> handles = system.add(&cubeNode->materials, MaterialType::PBR);
+    handles.append_range(system.add(&cubeNode->materials, MaterialType::PBR));
+    handles.append_range(system.add(&cubeNode->materials, MaterialType::PBR));
+    handles.append_range(system.add(&cubeNode->materials, MaterialType::PBR));
+    REQUIRE(0 < handles.size());
+    REQUIRE(0 == system.materials.size());
+
+    system.drainAdditionsInputBuffer();
+    std::vector<MaterialHandle> consumedHandles = system.getItemsAndDrainAdditionsOutputBuffer();
+    MaterialHandle materialToUpdate = consumedHandles[1];
+
+    MTL::Texture* textureToAdd = textureLoader.loadTexture("assets/test_cube_four_channel_texture.png");
+    REQUIRE(system.materials[materialToUpdate].pbrMaterial.albedoTexture != textureToAdd);
+
+    // When the call is made to update the material texture
+    system.updateMaterialTexture(materialToUpdate, (MaterialTextureOffset::TextureOffset)MaterialTextureOffset::PBRTextureOffset::Albedo, textureToAdd);
+    // The texture updates input buffer should have one item
+    REQUIRE(1 == system.textureUpdatesInputBuffer.count);
+    REQUIRE(textureToAdd != system.materials[materialToUpdate].pbrMaterial.albedoTexture);
+
+    // When the updates input buffer is drained
+    system.drainUpdatesInputBuffers();
+    // The texture updates input buffer should have zero items
+    REQUIRE(0 == system.textureUpdatesInputBuffer.count);
+
+    REQUIRE(textureToAdd == system.materials[materialToUpdate].pbrMaterial.albedoTexture);
+
+    // Cleaning up
+    system.drainRemovalsInputBuffer();
+    system.drainRemovalsOutputBufferAndUnloadResources();
+    aSceneIsFreed(scene);
+    metalDevice->release();
+    autoReleasePool->release();
+}
 
 TEST_CASE("updating a component of the ORM texture of a material should update the ORM texture of the material within the system", "[material][asset system][update][fbx][metal]") {}
