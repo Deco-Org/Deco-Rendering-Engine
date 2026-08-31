39a9ac925775a2873f50c3e424b3fddc287cc046
c-degni <christivanndeg@gmail.com>
2026-08-30 23:52:34 -0700

    range alloc

--- src/core_systems/animation_system.cpp
+++ src/core_systems/animation_system.cpp
@@ -4,3 +4,73 @@
  */
 
 #include "animation_system.hpp"
+
+AnimationSystem::AnimationSystem(MTL::Device *device = nullptr)
+{
+    
+}
+
+SkeletonHandle AnimationSystem::add_skeleton(SkeletonDescription description)
+{
+    
+}
+
+SkinBindingDescription AnimationSystem::add_skin_binding(SkinBindingDescription description)
+{
+    
+}
+
+ClipHandle AnimationSystem::add_clip(AnimationClipDescription description)
+{
+    
+}
+
+AnimationInstanceHandle AnimationSystem::add_instance(SkeletonHandle skeleton, SkinBindingHandle skin_binding)
+{
+    
+}
+
+void AnimationSystem::remove_instance(AnimationInstanceHandle instance)
+{
+    
+}
+
+void AnimationSystem::play(AnimationInstanceHandle instance, ClipHandle clip, const bool loop = false)
+{
+    
+}
+
+void AnimationSystem::stop(AnimationInstanceHandle instance)
+{
+    
+}
+
+void AnimationSystem::update(float delta_time)
+{
+    
+}
+
+void AnimationSystem::update_skinning_buffer(uint8_t buffer_index)
+{
+    
+}
+
+NS::UInteger AnimationSystem::skinning_buffer_offset(AnimationInstanceHandle instance) const
+{
+    
+}
+
+simd_quatf AnimationSystem::interpolate_rotation(simd_quatf q0, simd_quatf q1, float t)
+{
+    
+}
+
+void AnimationSystem::sample_track(
+    const BakedJointTrack& track, 
+    float time, 
+    simd_float3& out_transformation,
+    simd_float3& out_rotation,
+    simd_float3& out_scale)
+{
+    
+}
---
+++ src/tools/range_allocator.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file range_allocator.cpp
+ * @brief 
+ */
+
+#include "range_allocator.hpp"
+
+RangeAllocator::RangeAllocator(uint32_t capacity) : total_capacity(capacity)
+{
+    free_blocks.push_back({ 0, capacity });
+}
+
+std::optional<AllocationRange> RangeAllocator::alloc(uint32_t size, uint32_t alignment = 1)
+{
+    for (auto it = free_blocks.begin(); it != free_blocks.end(); ++it)
+    {
+        uint32_t aligned_start = ((it->start + alignment - 1) / alignment) * alignment;
+        uint32_t padding = aligned_start - it->start;
+
+        if (it->size < padding + size) continue;
+
+        AllocationRange range = { aligned_start, size };
+
+        if (!padding)
+        {
+            if (it->size == size)
+            {
+                free_blocks.erase(it);
+            }
+            else
+            {
+                it->start += size;
+                it->size -= size;
+            }
+        }
+        else
+        {
+            uint32_t block_size = it->size;
+            it->size = padding;
+
+            uint32_t leftover = block_size - (padding + size);
+            if (leftover > 0)
+            {
+                free_blocks.insert(it + 1, { aligned_start + size, leftover });
+            }
+        }
+
+        return range;
+    }
+
+    return std::nullopt;
+}
+
+void RangeAllocator::free(AllocationRange range)
+{
+    free_blocks.push_back({ range.start, range.size });
+    std::sort(free_blocks.begin(), free_blocks.end());
+
+    std::vector<FreeBlock> merged_blocks;
+
+    for (const auto& block : free_blocks)
+    {
+        if (merged_blocks.empty())
+        {
+            merged_blocks.push_back(block);
+        }
+        else
+        {
+            auto& last_block = merged_blocks.back();
+            if (last_block.start + last_block.size == block.start)
+            {
+                last_block.size += block.size;
+            }
+            else
+            {
+                // if last start + last size > block start, we overlapped blocks (alloc is bad)
+                // not asserting because I want to make logger first
+                merged_blocks.push_back(block);
+            }
+        }
+    }
+
+    free_blocks = std::move(merged_blocks);
+}
+
+uint32_t RangeAllocator::capacity() const
+{
+    return total_capacity;
+}
