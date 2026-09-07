9327f87d96822576cfa75ff8884b127419d294c5
Zachary <ZacharyScript@gmail.com>
2026-09-07 15:32:52 -0400

    Added command buffer creation on init of command allocator pool

--- tests/metal_backend/command_allocator_pool_test.cpp
+++ tests/metal_backend/command_allocator_pool_test.cpp
@@ -18,3 +18,18 @@ TEST_CASE("command allocator pool should init successfully", "[command allocator
     autorelease_pool->release();
     device->release();
 }
+
+TEST_CASE("the command allocator pool should create a command buffer on init", "[command allocator pool][metal]")
+{
+    NS::AutoreleasePool* autorelease_pool = NS::AutoreleasePool::alloc()->init();
+    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    MTL4::CommandQueue* command_queue = device->newMTL4CommandQueue();
+    CommandAllocatorPool* command_allocator_pool = new CommandAllocatorPool(device);
+
+    REQUIRE(nullptr != command_allocator_pool->get_command_buffer());
+
+    delete command_allocator_pool;
+    command_queue->release();
+    autorelease_pool->release();
+    device->release();
+}
