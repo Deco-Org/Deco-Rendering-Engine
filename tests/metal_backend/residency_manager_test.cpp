aa66cc7e4268995395f11803b6e9c032b6cb023b
Zachary <ZacharyScript@gmail.com>
2026-09-07 13:36:47 -0400

    Wrote test, adjusted cmakelists.txt

--- tests/metal_backend/residency_manager_test.cpp
+++ tests/metal_backend/residency_manager_test.cpp
@@ -2,11 +2,18 @@
 #include "test_utils.hpp"
 #include "metal_backend/residency_manager.hpp"
 
-TEST_CASE("residency manager should sucessfully create residency sets on init", "[residency manager][residency][metal]")
+TEST_CASE("residency manager should successfully init and deconstruct", "[residency manager][residency][metal]")
 {
     NS::AutoreleasePool* autorelease_pool = NS::AutoreleasePool::alloc()->init();
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    MTL4::CommandQueue* command_queue = device->newMTL4CommandQueue();
 
+    {
+        ResidencyManager residency_manager(device, command_queue);
+        REQUIRE(0 == residency_manager.latest_commit_value);
+    }
+
+    command_queue->release();
     autorelease_pool->release();
     device->release();
 }
