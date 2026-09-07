02d3aeef97e56d79c72e8f629a462b86d6711502
Zachary <ZacharyScript@gmail.com>
2026-09-07 16:46:52 -0400

    Added frame count increment and did some cleanup

--- src/metal_backend/command_allocator_pool.cpp
+++ src/metal_backend/command_allocator_pool.cpp
@@ -7,23 +7,53 @@
 
 CommandAllocatorPool::CommandAllocatorPool(MTL::Device* metal_device)
 {
+    frame_count = 0;
     command_buffer = metal_device->newCommandBuffer();
+    frame_event = metal_device->newSharedEvent();
+    frame_event->setSignaledValue(frame_count);
+
+    for (uint8_t i = 0; i < Config::MAX_FRAMES_IN_FLIGHT; ++i)
+    {
+        allocators[i] = metal_device->newCommandAllocator();
+    }
 }
 
 CommandAllocatorPool::~CommandAllocatorPool()
 {
-    release_command_buffer();
+    if (!command_buffer)
+        release_command_buffer();
 }
 
-MTL4::CommandBuffer* CommandAllocatorPool::get_command_buffer()
+void CommandAllocatorPool::begin_frame()
 {
-    return command_buffer;
+    if (frame_count >= Config::MAX_FRAMES_IN_FLIGHT)
+    {
+        uint64_t wait_value = frame_count - Config::MAX_FRAMES_IN_FLIGHT;
+        printf("wait value is %u\n", wait_value);
+        bool before_timeout = frame_event->waitUntilSignaledValue(frame_count, FRAME_TIMEOUT_TIME_IN_MILLISECONDS);
+    }
+
+    uint64_t frame_index = frame_count % Config::MAX_FRAMES_IN_FLIGHT;
+    allocators[frame_index]->reset();
+    command_buffer->beginCommandBuffer(allocators[frame_index]);
+
+    frame_count += 1;
+}
+
+void CommandAllocatorPool::signal_frame_complete(MTL4::CommandQueue* command_queue)
+{
+    command_queue->signalEvent(frame_event, frame_count);
 }
 
 void CommandAllocatorPool::release_command_buffer()
 {
     command_buffer->release();
     command_buffer = nullptr;
+}
+
+MTL4::CommandBuffer* CommandAllocatorPool::get_command_buffer()
+{
+    return command_buffer;
 }
 
 uint64_t CommandAllocatorPool::get_frame_count() const
--- tests/metal_backend/command_allocator_pool_test.cpp
+++ tests/metal_backend/command_allocator_pool_test.cpp
@@ -34,7 +34,7 @@ TEST_CASE("the command allocator pool should create a command buffer on init", "
     device->release();
 }
 
-TEST_CASE("the frame count should be zero on init", "[command allocator pool][metal]")
+TEST_CASE("the frame count should be zero on init", "[command allocator pool][frame][metal]")
 {
     NS::AutoreleasePool* autorelease_pool = NS::AutoreleasePool::alloc()->init();
     MTL::Device* device = MTL::CreateSystemDefaultDevice();
@@ -42,6 +42,34 @@ TEST_CASE("the frame count should be zero on init", "[command allocator pool][me
     CommandAllocatorPool* command_allocator_pool = new CommandAllocatorPool(device);
 
     REQUIRE(0 == command_allocator_pool->get_frame_count());
+
+    delete command_allocator_pool;
+    command_queue->release();
+    autorelease_pool->release();
+    device->release();
+}
+
+TEST_CASE("the frame count should increment upon the occurrence of the frame event", "[command allocator pool][frame][metal]")
+{
+    NS::AutoreleasePool* autorelease_pool = NS::AutoreleasePool::alloc()->init();
+    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    MTL4::CommandQueue* command_queue = device->newMTL4CommandQueue();
+    CommandAllocatorPool* command_allocator_pool = new CommandAllocatorPool(device);
+
+    REQUIRE(0 == command_allocator_pool->get_frame_count());
+
+    command_allocator_pool->begin_frame();
+    REQUIRE(1 == command_allocator_pool->get_frame_count());
+
+    MTL4::CommandBuffer* command_buffer = command_allocator_pool->get_command_buffer();
+    command_queue->commit(&command_buffer, 1);
+    command_allocator_pool->signal_frame_complete(command_queue);
+
+    command_allocator_pool->begin_frame();
+    REQUIRE(2 == command_allocator_pool->get_frame_count());
+
+    command_queue->commit(&command_buffer, 1);
+    command_allocator_pool->signal_frame_complete(command_queue);
 
     delete command_allocator_pool;
     command_queue->release();
